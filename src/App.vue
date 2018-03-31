efb4438b4911efd23d0f38fe79e6ef9a821960f0
Sura <375262986@qq.com>
2018-03-31 20:24:55 +0800

    feat:添加seller组件，完成项目

--- src/App.vue
+++ src/App.vue
@@ -12,28 +12,35 @@
         <router-link to="/seller">商家</router-link>
       </div>
     </div>
-    <router-view :seller="seller"></router-view>
+    <keep-alive>
+      <router-view :seller="seller"></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+import {urlParse} from '@/common/js/util'
 import header from './components/header/header.vue'
 
 const ERR_OK = 0
 
 export default {
   created () {
-    this.$http.get('/api/seller').then((res) => {
+    this.$http.get('/api/seller?id=' + this.seller.id).then((res) => {
       res = res.data
       if (res.errno === ERR_OK) {
-        this.seller = res.data
-        console.log(res.data)
+        this.seller = Object.assign({}, this.seller, res.data)
       }
     })
   },
   data () {
     return {
-      seller: {}
+      seller: {
+        id: (() => {
+          let queryParam = urlParse()
+          return queryParam.id
+        })()
+      }
     }
   },
   components: {
